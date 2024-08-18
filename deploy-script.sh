e0619621e8c1f747b9b1ef740028ade750c6b176
Yadu-M <yaduyem@gmail.com>
2024-08-18 18:57:52 -0400

    updating script

--- deploy-script.sh
+++ deploy-script.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
-set -e  # Exit script on any command failure
+set -e  # Exit on any error
 
 cd portfolio
 
-# Pull changes
 echo "Pulling Changes"
-git pull || { echo "Git pull failed"; exit 1; }
+git pull
 
-# Build the app
+npm i
+
 echo "Building App"
-npm run build || { echo "Build failed"; exit 1; }
+npm run build || { echo "Build failed"; cat /home/yadu/.npm/_logs/$(ls -t /home/yadu/.npm/_logs/ | head -n 1); exit 1; }
 
-# Kill the port before running the app
 echo "Killing server"
-fuser -k 3000/tcp || { echo "Failed to kill the server"; exit 1; }
+fuser -k 3000/tcp
 
 echo "Running app and ddns script"
-
-# Run the App
-nohup npm run start > npm_output.log 2>&1 & || { echo "Failed to start the app"; exit 1; }
-
-# Run NOIP
-nohup noip-duc -g all.ddnskey.com --username $NOIP_USERNAME --password $NOIP_PASSWORD > noip_output.log 2>&1 & || { echo "Failed to run NOIP"; exit 1; }
+nohup npm run start > npm_output.log 2>&1 &
+nohup noip-duc -g all.ddnskey.com --username $NOIP_USERNAME --password $NOIP_PASSWORD > noip_output.log 2>&1 &
 
 echo "The scripts are now running in the background. You can close the terminal."
