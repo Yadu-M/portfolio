2c7582b3aeb693077a24e69fc389e0d6f18ff051
Yadu-M <yaduyem@gmail.com>
2024-08-28 00:55:40 -0400

    updated description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,14 +38,14 @@ export default function Home() {
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
         <h2 className="text-sm text-zinc-500 ">
-          I&apos;m building an {" "}
+          I&apos;m building {" "}
           <Link
             target="_blank"
-            href="https://github.com/namoosir/gemini-contest"
+            href="https://github.com/Yadu-M/events_TO"
             className="underline duration-500 hover:text-zinc-300"
           >
-            AI Interviewer
-          </Link> to assist job seekers for interview preparation.
+            Events TO
+          </Link> to showcase events happening around Toronto.
         </h2>
       </div>
     </div>
